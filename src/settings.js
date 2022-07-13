f29f0cf08e8fcc6bc927caaba8fdb869a57b948a
GKPaaa <1420361683@qq.com>
2022-07-13 11:49:46 +0800

    '项目初始化'

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: 'GKP vue 人资',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置appmain右侧主体上的面包屑是不是固定定位
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 设置侧边栏上面的logo是否演示
+   */
+  sidebarLogo: true
+}
